f93cf54908e0c046b215a65e256de3a72f5eced4
LeoKu <czc12580520@gmail.com>
2023-03-06 22:09:02 +0800

    feat: new ui

--- src/components/ui/MessageItem.tsx
+++ src/components/ui/MessageItem.tsx
@@ -30,7 +30,7 @@ export default ({ role, message }: Props) => {
         background: 'linear-gradient(90deg, #010004 0%, rgb(148 163 184) 50%, #010004 100%)'
       }} />}
 
-      <div class="flex gap-3 md:gap-6 pl-3 pr-5 py-5 md:py-10 md:px-20 rounded-lg">
+      <div class={`flex gap-3 md:gap-6 pl-3 pr-5 md:px-20 rounded-lg ${role === 'user' ? 'py-3 md:py-6' : 'py-5 md:py-10'}`}>
         <div class={`shrink-0 flex items-center justify-center w-6 h-6 md:w-10 md:h-10 mt-4 md:mt-2.5 select-none rounded-md md:rounded-lg ${role === 'user' ? 'bg-#1b2429' : 'bg-#ccf5cf text-#010004 md:p-2 p-1'}`}>
           {role === 'user' 
           ? <span class="hidden md:inline text-xs">YOU</span>
@@ -43,15 +43,6 @@ export default ({ role, message }: Props) => {
       {role!=='user' && <div class="h-1px opacity-30" style={{
         background: 'linear-gradient(90deg, #010004 0%, rgb(148 163 184) 50%, #010004 100%)'
       }} />}
-
-      {/* { showRetry?.() && onRetry && (
-        <div class="flex items-center justify-end px-3 mb-2">
-          <div onClick={onRetry} class="flex items-center gap-1 px-2 py-0.5 op-70 border border-slate text-slate rounded-md text-sm cursor-pointer hover:bg-slate/10">
-            <IconRefresh />
-            <span>Regenerate</span>
-          </div>
-        </div>
-      )} */}
     </div>
   )
 }
